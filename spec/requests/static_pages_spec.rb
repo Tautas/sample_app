652289a00c0b1f56da7b5d5bff67ca158e3b7b24
Tautas <Tautvydas.jazbutis@gmail.com>
2012-04-29 19:25:13 +0100

    Added contact page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -43,4 +43,19 @@ describe "StaticPages" do
   			:text => "Ruby on Rails Tutorial Sample App | About Us")  
   	end
   end
+
+  describe "Contact Page" do
+    
+    it "should have the h1 'Contact Page'" do
+      visit '/static_pages/contact'
+      page.should have_selector('h1', :text=> 'Contact')
+    end
+    
+    it "should have the title 'Contact'" do
+    	visit '/static_pages/contact'
+    	page.should have_selector('title',
+    		:text => "Ruby on Rails Tutorial Sample App | Contact")
+    end
+  end
+
 end
